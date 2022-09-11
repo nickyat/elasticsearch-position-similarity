8f3dc0ef8fd30673197865ac12ce92b92a5c825c
Яцуков Николай <y@adeo.pro>
2022-09-11 16:07:30 +0300

    8.4.1

--- docker/dev.sh
+++ docker/dev.sh
@@ -5,4 +5,4 @@ docker run  --name es8-dev -p 9200:9200 -p 9300:9300 -p 5005:5005 \
  -v `pwd`/plugins:/plugins \
  -e "xpack.security.enabled=false" \
  -e ES_JAVA_OPTS="-Xms1g -Xmx1g -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:5005 " \
-  docker-hub.adeo.pro/elasticsearch-rm:8.1.3
+  docker-hub.adeo.pro/elasticsearch-rm:8.4.1
